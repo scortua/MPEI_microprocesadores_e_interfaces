ff942be4c4ed188c47e3a88115ed465945d13b4f
santiago cortes tovar <scortua@hotmail.com>
2024-04-27 14:05:50 -0500

    pwm qei

--- PWM_QEI.X/main.c
+++ PWM_QEI.X/main.c
@@ -1,12 +1,13 @@
-#pragma config FNOSC = FRC  // Selecciona el oscilador interno r?pido (FRC)
+ #pragma config FNOSC = FRC  // Selecciona el oscilador interno r?pido (FRC)
 #pragma config IESO = ON    // Habilita el cambio de oscilador interno/externo
 #pragma config FWDTEN = OFF // Deshabilita el watchdog timer
 #pragma config ICS = PGD3   // Selecciona el canal de comunicaci?n PGD3
 
-#include "xc.h"
+
 #include "p33FJ128MC802.h"
 #define FCY 7372800
 #include <libpic30.h>
+#include "xc.h"
 
 #define FP  FCY/2
 #define BAUDRATE 19200
@@ -18,41 +19,30 @@ void ADC_conf();
 void adquirir();
 void UART_conf();
 void conf_pwm();
+void QEI_conf();
 
-int lectura;
+int lectura=0;
 double m = 14.4549;
-
-int duty;
+int duty=0;
 
 int main(void) {
     AD1PCFGL = 0xFFFB; // 1111 1111 1111 1011
-    TRISB = 0x0001;
+    TRISB = 0x0005; // 0000 0000 0000 0101
+    RPINR14bits.QEA1R = 2; // entrada por RP2
+    RPOR0bits.RP1R = 3; // Salida UART1 por RP1
     ADC_conf();
-    UART_conf();
     conf_pwm();
+    QEI_conf();
+    //UART_conf();
+    
     while(1){
         adquirir();
-        duty = m*lectura;
+        duty = m*lectura;//0X0E66-lectura*0X000E;
         P2DC1 = duty;
+        U1TXREG = POS1CNT;
     }
     
     return 0;
-}
-
-void UART_conf() {
-    U1MODEbits.STSEL = 0; // 1-Stop bit        
-    U1MODEbits.PDSEL = 0; // No Parity, 8-Data bits
-    U1MODEbits.ABAUD = 0; // Auto-Baud disabled
-    U1MODEbits.BRGH = 0; // Standard-Speed mode
-
-    U1BRG = BRGVAL; // Baud Rate setting for monda
-
-    U1STAbits.UTXISEL1 = 0; // Interrupt after one RX character is received   
-    IEC0bits.U1RXIE = 1; // Enable UART RX interrupt
-
-    U1MODEbits.UARTEN = 1; // Enable UART
-    U1STAbits.UTXEN = 1; // Enable Transmition UART    
-
 }
 
 void ADC_conf() {
@@ -69,11 +59,11 @@ void ADC_conf() {
 
 void adquirir() {
     AD1CON1bits.SAMP = 1;
-    __delay_ms(100);
+    __delay_ms(1);
     AD1CON1bits.SAMP = 0;
     while (!AD1CON1bits.DONE);
-    lectura = ADCBUF0 >> 2;
-    __delay_ms(5);
+    lectura = ADCBUF0 >> 2; 
+    lectura = lectura & (0x03FF);
 }
 
 void conf_pwm(){
@@ -88,4 +78,30 @@ void conf_pwm(){
     P2DTCON1bits.DTA = 59;       // tiempo muerto 16us
     P2DC1 = 0x0000; 
     P2TCONbits.PTEN = 1;            // enable PWM timerbase
+}
+
+void QEI_conf(){
+    QEI1CON = 0;
+    QEI1CONbits.UPDN = 1;
+    QEI1CONbits.QEIM = 5;
+    QEI1CONbits.TQGATE = 1;
+    DFLT1CON = 0;
+    DFLT1CONbits.QEOUT = 1;
+    MAX1CNT = 256;
+}
+
+ void UART_conf() {
+    U1MODEbits.STSEL = 0; // 1-Stop bit        
+    U1MODEbits.PDSEL = 0; // No Parity, 8-Data bits
+    U1MODEbits.ABAUD = 0; // Auto-Baud disabled
+    U1MODEbits.BRGH = 0; // Standard-Speed mode
+
+    U1BRG = BRGVAL; // Baud Rate setting for monda
+
+    //U1STAbits.UTXISEL1 = 0; // Interrupt after one RX character is received   
+    //IEC0bits.U1RXIE = 1; // Enable UART RX interrupt
+
+    U1MODEbits.UARTEN = 1; // Enable UART
+    U1STAbits.UTXEN = 1; // Enable Transmition UART    
+
 }
